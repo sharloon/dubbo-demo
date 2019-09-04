663277b8881bd3ffbcda4c14a8891da664916993
huangxiaolong <sharloon@163.com>
2019-09-04 15:30:15 +0800

    1.配置dubbo相关的配置文件 2.实现client-service通过dubbo远程调用order-service

---
+++ client-service/src/main/java/sut/hxl/dubbodemo/clientservice/controller/TestController.java
@@ -0,0 +1,27 @@
+package sut.hxl.dubbodemo.clientservice.controller;
+
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import stu.hxl.dubbodemo.api.IOrderService;
+
+/**
+ * @author sharloon
+ * @date 2019/9/4
+ */
+@RestController
+public class TestController {
+
+    @Reference
+    private IOrderService orderService;
+
+    @GetMapping("/test")
+    public String test(@RequestParam String no) {
+
+        System.out.println("test success ~");
+
+        return orderService.queryOrder(no);
+    }
+
+}
--- order-service/order-provider/src/main/java/stu/hxl/dubbodemo/provider/OrderServiceImpl.java
+++ order-service/order-provider/src/main/java/stu/hxl/dubbodemo/provider/OrderServiceImpl.java
@@ -1,16 +1,17 @@
 package stu.hxl.dubbodemo.provider;
 
+import org.apache.dubbo.config.annotation.Service;
 import stu.hxl.dubbodemo.api.IOrderService;
 
 /**
  * @author sharloon
  * @date 2019/9/4
  */
+@Service
 public class OrderServiceImpl implements IOrderService {
 
     @Override
     public String queryOrder(String no) {
-
         return "订单查询成功，订单号为：" + no;
     }
 }
